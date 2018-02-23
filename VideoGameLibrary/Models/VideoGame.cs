86d9eebc59a52fdf548551938396b4d861e1495e
evan686z <friise@mail.nmc.edu>
2018-02-23 00:05:54 -0500

    Added Validation with Regex in the VideoGame model. Updated Create and Edit view to show an example of a YouTube embed link.

--- VideoGameLibrary/Models/VideoGame.cs
+++ VideoGameLibrary/Models/VideoGame.cs
@@ -13,12 +13,15 @@ namespace VideoGameLibrary.Models
         [Display(Name = "Title")]
         public string Name { get; set; }
         [Display(Name = "Release Date")]
+        [RegularExpression(@"(Jan(uary)?|Feb(ruary)?|Mar(ch)?|Apr(il)?|May|Jun(e)?|Jul(y)?|Aug(ust)?|Sep(tember)?|Oct(ober)?|Nov(ember)?|Dec(ember)?)\s+\d{1,2},\s+\d{4}", ErrorMessage = "Please enter a valid date (Month Name, Day, Year).")]
         public string ReleaseDate { get; set; }
         [Display(Name = "Release Year")]
+        [RegularExpression(@"^\d{4}$", ErrorMessage = "Please Enter a 4 digit year.")]
         public string ReleaseYear { get; set; }
         public string Developer { get; set; }
         public string Publisher { get; set; }
         [Display(Name = "Embeded YouTube Link")]
+        [RegularExpression(@"(?:https?:\/\/)?(?:www\.)?youtu\.?be(?:\.com)?\/?.*(?:watch|embed)?(?:.*v=|v\/|\/)([\w\-_]+)\&?", ErrorMessage = "Please Enter a valid YouTube src embed link.")]
         public string YouTubeEmbedLink { get; set; }
     }
 }
